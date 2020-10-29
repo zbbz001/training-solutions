bbaf5fc86e1e9e37bab4beec45bd7134c27d7b82
Erős Attila <eros.at@gmail.com>
2020-10-29 21:10:13 +0100

    Kész

---
+++ src/main/java/classsctructuremethods/NoteMain.java
@@ -0,0 +1,13 @@
+package classsctructuremethods;
+
+public class NoteMain {
+    public static void main(String[] args) {
+        Note note = new Note();
+        note.setName("Cody Coder");
+        note.setTopic("Top Secret");
+        note.setText("xxxxxx xxxxxxxx xxxxxxxxx xxxxxx");
+
+        System.out.println("Na nézzük, hogy ki mit írt!");
+        System.out.println(note.getNoteText());
+    }
+}
