f9b5a05e40c6a030b2b04148dea68f2eb18faf2b
Erős Attila <eros.at@gmail.com>
2020-11-02 16:36:16 +0100

    Adatbekéréssel kiegészítve

--- src/main/java/classsctructureintegrate/BankAccountMain.java
+++ src/main/java/classsctructureintegrate/BankAccountMain.java
@@ -1,37 +1,56 @@
 package classsctructureintegrate;
 
+import java.util.Scanner;
+
 public class BankAccountMain {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        BankAccount customerA = new BankAccount("000-000-001", "Cody Coder", 888888);
-        BankAccount customerB = new BankAccount("900-900-900", "Rusty Hook", 700700);
+        System.out.println("Hogy hívják?");
+        String accountOwnerA = scanner.nextLine();
 
+        System.out.println("Mennyi pénzzel szeretné megnyiti a számlát?");
+        int accountBalanceA = scanner.nextInt();
 
-        System.out.println("Na nézzük kinek mennyi pénze van:");
-        System.out.println(customerA.getInfo());
-        System.out.println(customerB.getInfo());
+        BankAccount accountA = new BankAccount("000-000-001", accountOwnerA, accountBalanceA);
+        System.out.println(accountA.getInfo());
+        scanner.nextLine();
         System.out.println();
 
-        System.out.println("Adjunk mindenkinek 5555 Ft-ot, és nézzük meg újra az egyenleget");
-        customerA.deposit(5555);
-        customerB.deposit(5555);
-        System.out.println(customerA.getInfo());
-        System.out.println(customerB.getInfo());
+
+        System.out.println("Hogy hívják?");
+        String accountOwnerB = scanner.nextLine();
+
+        System.out.println("Mennyi pénzzel szeretné megnyiti a számlát?");
+        int accountBalanceB = scanner.nextInt();
+
+        BankAccount accountB = new BankAccount("900-900-900", accountOwnerB, accountBalanceB);
+        System.out.println(accountB.getInfo());
+        scanner.nextLine();
+        System.out.println();
+
+
+        System.out.println(accountOwnerA + ", mennyi szeretne utalni " + accountOwnerB + "-nek?");
+        int transferAmount = scanner.nextInt();
+        accountA.transfer(accountB, transferAmount);
+
+        System.out.println(accountOwnerB + ", mennyi szeretne felvenni a számlájáról?");
+        int withdraw = scanner.nextInt();
+        accountB.withdraw(withdraw);
+        scanner.nextLine();
         System.out.println();
 
         System.out.println("Persze, mindenki elfelejtett befizetni a számláját!");
         System.out.println("Vonjuk hát le tölük a 417 Ft-ot, és nézzük meg mi maradt!");
-        customerA.withdraw(417);
-        customerB.withdraw(417);
-        System.out.println(customerA.getInfo());
-        System.out.println(customerB.getInfo());
+        accountA.withdraw(417);
+        accountB.withdraw(417);
         System.out.println();
 
-        System.out.println(customerA.getOwner() + " egy arany ember, kölcsön adott 9417 Ft-ot " + customerB.getOwner() + "-nak, és így a számlájuk:");
-        customerA.transfer(customerB, 9417);
-        System.out.println(customerA.getInfo());
-        System.out.println(customerB.getInfo());
+        System.out.println("Nap végi egyenlegek:");
+        System.out.println(accountA.getInfo());
+        System.out.println(accountB.getInfo());
         System.out.println();
+
     }
 }
