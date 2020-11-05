44a7ea9f231ab9e717e980b81187a5b975c4582e
Erős Attila <eros.at@gmail.com>
2020-11-05 15:39:27 +0100

    Week02, 3. feladat Kész

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,21 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputNames {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = new String[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Adja meg a nevét: ");
+            names[i] = scanner.nextLine();
+        }
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(i + 1 + ". elem: " + names[i]);
+        }
+    }
+}
