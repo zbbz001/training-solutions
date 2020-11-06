d0b73854221f71560855895f85e2b51714ae24da
Erős Attila <eros.at@gmail.com>
2020-11-06 15:42:38 +0100

    Week02,4.Feladat Kész

--- src/main/java/week02/Languages.java
+++ src/main/java/week02/Languages.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class Languages {
 
     public static void main(String[] args) {
-        List<String> lista  = new ArrayList<>();
+        List<String> progList  = new ArrayList<>();
 
-        lista.add("Java");
-        lista.add("Python");
-        lista.add("JavaScript");
+        progList.add("Java");
+        progList.add("Python");
+        progList.add("JavaScript");
 
-        for (String progLang: lista ) {
+        for (String progLang: progList ) {
             if (progLang.length() > 5) {
                 System.out.println(progLang);
             }
