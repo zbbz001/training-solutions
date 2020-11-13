06230d7516c7fecda99c9fed6cc30be9bc5a6885
Erős Attila <eros.at@gmail.com>
2020-11-13 15:52:59 +0100

    Week03 feladat2 Kész

---
+++ src/main/java/week03/Operation.java
@@ -0,0 +1,15 @@
+package week03;
+
+public class Operation {
+    private int left;
+    private int right;
+
+    public Operation(String str) {
+        left = Integer.parseInt(str.substring(0, str.indexOf("+")).trim());
+        right = Integer.parseInt(str.substring(str.indexOf("+") + 1, str.length()).trim());
+    }
+
+    public int getResult() {
+        return left + right;
+    }
+}
---
+++ src/test/java/week03/OperationTest.java
@@ -0,0 +1,15 @@
+package week03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OperationTest {
+
+    @Test
+    public void testGetResult() {
+        Operation operation = new Operation("11+22");
+
+        assertEquals(33, operation.getResult());
+    }
+}
