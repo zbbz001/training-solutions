252145387eacfe771b3ba79d1b19348d620952f5
Erős Attila <eros.at@gmail.com>
2020-11-30 00:31:13 +0100

    Kész

---
+++ src/main/java/methodargvars/examstats/ExamStats.java
@@ -0,0 +1,29 @@
+package methodargvars.examstats;
+
+public class ExamStats {
+    private int maxPoints;
+
+    public ExamStats(int maxPoints) {
+        this.maxPoints = maxPoints;
+    }
+
+    public int getNumberOfTopGrades(int limitInPercent, int... results) {
+        if (results == null || results.length == 0) {
+            throw new IllegalArgumentException("Number of results must not be empty!");
+        }
+
+        int counter = 0;
+
+        for (int result : results) {
+            if (result >= maxPoints * limitInPercent / 100) {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
+    public boolean hasAnyFailed(int limitInPercent, int... results) {
+        return getNumberOfTopGrades(limitInPercent, results) != results.length;
+    }
+}
