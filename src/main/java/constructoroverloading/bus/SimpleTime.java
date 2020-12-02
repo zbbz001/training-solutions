fbb7ce34f062d5ac041a9b1d17c0b5bafc525f0a
Erős Attila <eros.at@gmail.com>
2020-12-02 10:39:54 +0100

    kész

---
+++ src/main/java/constructoroverloading/bus/SimpleTime.java
@@ -0,0 +1,42 @@
+package constructoroverloading.bus;
+
+public class SimpleTime {
+    private int hours;
+    private int minutes;
+
+    public SimpleTime(int hours, int minutes) {
+        this.hours = hours;
+        this.minutes = minutes;
+    }
+
+    public SimpleTime(int hours) {
+        this(hours, 0);
+    }
+
+    public SimpleTime(SimpleTime time) {
+        this(time.getHours(), time.getMinutes());
+    }
+
+    public int difference(SimpleTime time) {
+        return convertTimeToMinutes() - time.convertTimeToMinutes();
+    }
+
+    public int convertTimeToMinutes() {
+        return hours * 60 + minutes;
+    }
+
+    @Override
+    public String toString() {
+        return hours + ":" + minutes;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+
+}
